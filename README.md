cb24a6f2c263f4c38191c63ca897e1b290cc6952
WooKyoung Noh <wookay.noh@gmail.com>
2026-03-26 14:18:57 +0900

    update repositories

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# TestLLM 💬
+# TestLLM.jl 💬
 
 [![CI](https://github.com/wookay/TestLLM.jl/actions/workflows/actions.yml/badge.svg)](https://github.com/wookay/TestLLM.jl/actions/workflows/actions.yml)
+
+### repositories
+ - TestCompiler 🚗  https://github.com/wookay/TestCompiler.jl
+ - TestJuliaLowering 📉  https://github.com/wookay/TestJuliaLowering.jl
+ - TestStdlibs 🏫  https://github.com/wookay/TestStdlibs.jl
+ - TestLLM 💬  https://github.com/wookay/TestLLM.jl
+ - TestPkgs 📦  https://github.com/wookay/TestPkgs.jl
--- test/llama2/karpathy.jl
+++ test/llama2/karpathy.jl
@@ -1,17 +1,17 @@
 module test_llama2_karpathy
 
+using Test
 using Llama2
 load_karpathy_model
 sample
 
-using Test
-using Pkg.Artifacts: artifact_path
-using LanguageModels: stories42M_model # ::SHA1
-const default_model = artifact_path(stories42M_model)
+# using Pkg.Artifacts: artifact_path
+# using LanguageModels: stories42M_model # ::SHA1
+# const default_model = artifact_path(stories42M_model)
 
-format = "tinyllamas"
-checkpoint_filename = joinpath(default_model, "stories42M.bin")
-tokenizer_filename = joinpath(default_model, "tokenizer.model")
+# format = "tinyllamas"
+# checkpoint_filename = joinpath(default_model, "stories42M.bin")
+# tokenizer_filename = joinpath(default_model, "tokenizer.model")
 
 # Convert the tokenizer.model into .bin file:
 # https://raw.githubusercontent.com/karpathy/llama2.c/master/tokenizer.py
