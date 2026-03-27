6d6a483b1ce10700bc2a5c1e209cd7d9da6ef80c
WooKyoung Noh <wookay.noh@gmail.com>
2026-03-27 09:24:47 +0900

    test promptingtools, zana

--- test/promptingtools/ollama.jl
+++ test/promptingtools/ollama.jl
@@ -2,9 +2,20 @@ module test_promptingtools_ollama
 
 using Test
 using PromptingTools: PromptingTools as PT
-using .PT: OllamaSchema
 
-schema = OllamaSchema()
-@info schema
+schema = PT.OllamaSchema()
+prompt = "hi"
+
+on_ci = haskey(ENV, "CI")
+if !on_ci
+conversation = PT.aigenerate(schema,
+                             prompt ;
+                             verbose = true,
+                             model = "mistral",
+                             return_all = true)
+@test conversation[1] isa PT.SystemMessage
+@test conversation[2] isa PT.UserMessage
+@test conversation[3] isa PT.AIMessage
+end # if
 
 end # module test_promptingtools_ollama
--- test/zana/agent.jl
+++ test/zana/agent.jl
@@ -1,6 +1,14 @@
+module test_zana_agent
+
+using Test
 using Zana
+
 config = ZanaConfig(
     ollama = OllamaConfig(model="mistral")
 )
+
 agent = ZanaAgent(config, ".")
-@info agent
+
+@test agent.config.ollama.host == "http://localhost:11434"
+
+end # module test_zana_agent
