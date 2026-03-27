d03ed4e33f85afdfc3851df0aad14e29cbdd8372
WooKyoung Noh <wookay.noh@gmail.com>
2026-03-27 09:40:16 +0900

    test bytepairencoding

--- test/bytepairencoding/learn.jl
+++ test/bytepairencoding/learn.jl
@@ -1,19 +1,21 @@
-using Jive
-@useinside Main module test_bytepairencoding_learn
+module test_bytepairencoding_learn
 
-# BytePairEncoding.jl/test/test_learn.jl
-#                          test_bpe.jl
-#                          test_bbpe.jl
-using BytePairEncoding: BytePairEncoding, TextEncodeBase # modules
-using .BytePairEncoding: BPETokenization, NoBPE, Merge, GPT2Tokenization, learn, rank2list
-using .TextEncodeBase: FlatTokenizer, CodeNormalizer, Sentence
+using Test
+using BytePairEncoding: BytePairEncoding as BPE, TextEncodeBase as TEB
+using .BPE: BPETokenization, NoBPE, Merge, GPT2Tokenization, learn, rank2list
 
-BPETokenization
-NoBPE()
-Merge("a")
-learn
-rank2list
+# from BytePairEncoding.jl/test/test_learn.jl
+#                               test_bpe.jl
+#                               test_bbpe.jl
 
-Sentence("hello world")
+BPE.BPETokenization
+BPE.NoBPE()
+BPE.Merge("a")
+BPE.learn
+BPE.rank2list
+
+sentence = TEB.Sentence("hello world")
+@test sentence.x == "hello world"
+@test sentence.meta === nothing
 
 end # module test_bytepairencoding_learn
